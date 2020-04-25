5e4f0ffc9e17656426700c2062d4fa17e7beeaf9
Alistair Deneys <alistair.deneys@gmail.com>
2020-04-25 16:38:54 +1000

    Added model without Item

---
+++ UnitTest/Comparers/CategoryComparer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnitTestingSitecoreComponents.Web.Models;
+
+namespace UnitTest.Comparers
+{
+    class CategoryComparer : IEqualityComparer<Category>
+    {
+        public bool Equals(Category x, Category y)
+        {
+            return
+                x.Title.Equals(y.Title) &&
+                x.Url.Equals(y.Url);
+        }
+
+        public int GetHashCode(Category obj)
+        {
+            return
+                obj.Title.GetHashCode() +
+                obj.Url.GetHashCode();
+        }
+    }
+}
---
+++ Web/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace UnitTestingSitecoreComponents.Web.Models
+{
+    public class Category
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+    }
+}
